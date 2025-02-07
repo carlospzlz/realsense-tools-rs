15bfeff88ed43c103be0a9b9d0ef95893b09e398
Carlos Perez-Lopez <carlospzlz@gmail.com>
2025-02-07 21:02:34 +0100

    Add some camera info to GUI

--- src/realsense_viewer.rs
+++ src/realsense_viewer.rs
@@ -1,30 +1,103 @@
 use eframe::egui;
+use std::collections::HashSet;
+
+/// Gets info from a device or returns "N/A" if a provided info parameter
+/// doesn't exist for the provided device.
+fn match_info(
+    device: &realsense_rust::device::Device,
+    info_param: realsense_rust::kind::Rs2CameraInfo,
+) -> String {
+    match device.info(info_param) {
+        Some(s) => String::from(s.to_str().unwrap()),
+        None => String::from("N/A"),
+    }
+}
 
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let realsense_ctx =
+        realsense_rust::context::Context::new().expect("Failed to create RealSense context");
 
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
         ..Default::default()
     };
+    eframe::run_native(
+        "Realsense Viewer \u{1F980}",
+        options,
+        Box::new(|cc| Ok(Box::new(MyApp::new(cc, realsense_ctx)))),
+    )
+}
 
-    // Our application state:
-    let mut name = "Arthur".to_owned();
-    let mut age = 42;
+struct MyApp {
+    name: String,
+    age: u8,
+    realsense_ctx: realsense_rust::context::Context,
+}
 
-    eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
-        egui::CentralPanel::default().show(ctx, |ui| {
+impl MyApp {
+    fn new(
+        _cc: &eframe::CreationContext<'_>,
+        realsense_ctx: realsense_rust::context::Context,
+    ) -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+            realsense_ctx,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, egui_ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let devices = self.realsense_ctx.query_devices(HashSet::new());
+        egui::CentralPanel::default().show(egui_ctx, |ui| {
             ui.heading("My egui Application");
             ui.horizontal(|ui| {
                 let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut name)
+                ui.text_edit_singleline(&mut self.name)
                     .labelled_by(name_label.id);
             });
-            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
             if ui.button("Increment").clicked() {
-                age += 1;
+                self.age += 1;
             }
-            ui.label(format!("Hello '{name}', age {age}"));
+            ui.label(format!("Hello '{0}', age {1}", self.name, self.age));
+            ui.label(format!("Devices found: {0}", devices.len()));
+
+            egui::Grid::new("devices").striped(true).show(ui, |ui| {
+                // Header
+                ui.label("Index");
+                ui.label("Name");
+                ui.label("Serial Number");
+                ui.label("Firmware Version");
+                ui.label("Recommended");
+                ui.end_row();
+
+                for (index, device) in devices.iter().enumerate() {
+                    ui.label(index.to_string());
+                    ui.label(match_info(
+                        &device,
+                        realsense_rust::kind::Rs2CameraInfo::Name,
+                    ));
+                    ui.label(match_info(
+                        &device,
+                        realsense_rust::kind::Rs2CameraInfo::SerialNumber,
+                    ));
+                    ui.label(match_info(
+                        &device,
+                        realsense_rust::kind::Rs2CameraInfo::FirmwareVersion,
+                    ));
+                    ui.label(match_info(
+                        &device,
+                        realsense_rust::kind::Rs2CameraInfo::RecommendedFirmwareVersion,
+                    ));
+                    ui.end_row();
+                }
+            });
         });
-    })
+
+        egui_ctx.request_repaint();
+    }
 }
